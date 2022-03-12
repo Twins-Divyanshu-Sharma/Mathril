a5e71deef202a424bd9746bec01e0f6dc0b1c40e
Twins-Diwakar-Sharma <Twins-Diwakar-Sharma@github.com>
2022-03-12 15:14:26 +0530

    Mat4 completed

--- incsrc/Mathril.cpp
+++ incsrc/Mathril.cpp
@@ -274,7 +274,7 @@ Mat2::Mat2()
 
 Mat2::~Mat2()
 {
-    if(!data)
+    if(data)
     {
         for(int i=0; i<2; i++)
             delete [] data[i];
@@ -505,6 +505,299 @@ std::ostream& operator<<(std::ostream& os, const Mat2& m)
     for(int i=0; i<2; i++)
     {
         for(int j=0; j<2; j++)
+        {
+            os << m.data[i][j] <<", ";
+        }
+        os << std::endl;
+    }
+
+    return os;
+}
+
+Mat4::Mat4()
+{
+    data = new float*[4];
+    for(int i=0; i<4; i++)
+    {
+        data[i] = new float[4];
+        for(int j=0; j<4; j++)
+            data[i][j] = 0;
+    }
+}
+
+Mat4::~Mat4()
+{
+    if(data)
+    {
+        for(int i=0; i<4; i++)
+            delete [] data[i];
+        delete [] data;
+    }
+}
+
+Mat4::Mat4(
+     float m00, float m01, float m02, float m03,
+     float m10, float m11, float m12, float m13,
+     float m20, float m21, float m22, float m23,
+     float m30, float m31, float m32, float m33
+                                                )
+{
+    data = new float*[4];
+    for(int i=0; i<4; i++)
+        data[i] = new float[4];
+
+    data[0][0] = m00;   data[0][1] = m01;   data[0][2] = m02;   data[0][3] = m03;
+    data[1][0] = m10;   data[1][1] = m11;   data[1][2] = m12;   data[1][3] = m13;
+    data[2][0] = m20;   data[2][1] = m21;   data[2][2] = m22;   data[2][3] = m23;
+    data[3][0] = m30;   data[3][1] = m31;   data[3][2] = m32;   data[3][3] = m33;
+}
+
+Mat4::Mat4(Mat4& m)
+{
+    data = new float*[4];
+    for(int i=0; i<4; i++)
+    {
+        data[i] = new float[4];
+        for(int j=0; j<4; j++)
+            data[i][j] = m.data[i][j];
+    }
+}
+
+Mat4::Mat4(Mat4&& m)
+{
+    data = m.data;
+    m.data = nullptr;
+}
+
+Mat4& Mat4::operator=(Mat4& m)
+{
+    if( this == &m ) return *this;
+
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            data[i][j] = m.data[i][j];
+
+    return *this;
+}
+
+Mat4& Mat4::operator=(Mat4&& m)
+{
+    float** temp = data;
+    data = m.data;
+    m.data = temp;
+    return *this;
+}
+
+float* Mat4::operator[](int n)
+{
+    return data[n];
+}
+
+Mat4 operator+(Mat4& m, Mat4& w)
+{
+    return Mat4(
+        m.data[0][0]+w.data[0][0], m.data[0][1]+w.data[0][1], m.data[0][2]+w.data[0][2], m.data[0][3]+w.data[0][3],
+        m.data[1][0]+w.data[1][0], m.data[1][1]+w.data[1][1], m.data[1][2]+w.data[1][2], m.data[1][3]+w.data[1][3],
+        m.data[2][0]+w.data[2][0], m.data[2][1]+w.data[2][1], m.data[2][2]+w.data[2][2], m.data[2][3]+w.data[2][3],
+        m.data[3][0]+w.data[3][0], m.data[3][1]+w.data[3][1], m.data[3][2]+w.data[3][2], m.data[3][3]+w.data[3][3]
+    );
+} 
+
+Mat4 operator+(Mat4& m, Mat4&& w)
+{
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            w[i][j] = m[i][j] + w[i][j];
+    return std::move(w);
+}
+
+Mat4 operator+(Mat4&& m, Mat4& w)
+{
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            m[i][j] = m[i][j] + w[i][j];
+    return std::move(m);
+}
+
+Mat4 operator+(Mat4&& m, Mat4&& w)
+{
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            m[i][j] = m[i][j] + w[i][j];
+    return std::move(m);
+}
+
+Mat4 operator-(Mat4& m, Mat4& w)
+{
+    return Mat4(
+            m.data[0][0]-w.data[0][0], m.data[0][1]-w.data[0][1], m.data[0][2]-w.data[0][2], m.data[0][3]-w.data[0][3],
+            m.data[1][0]-w.data[1][0], m.data[1][1]-w.data[1][1], m.data[1][2]-w.data[1][2], m.data[1][3]-w.data[1][3],
+            m.data[2][0]-w.data[2][0], m.data[2][1]-w.data[2][1], m.data[2][2]-w.data[2][2], m.data[2][3]-w.data[2][3],
+            m.data[3][0]-w.data[3][0], m.data[3][1]-w.data[3][1], m.data[3][2]-w.data[3][2], m.data[3][3]-w.data[3][3]
+        );
+}
+
+Mat4 operator-(Mat4& m, Mat4&& w)
+{
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            w[i][j] = m[i][j] - w[i][j];
+    return std::move(w);
+}
+
+Mat4 operator-(Mat4&& m, Mat4& w)
+{
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            m[i][j] = m[i][j] - w[i][j];
+    return std::move(m);
+}
+
+Mat4 operator-(Mat4&& m, Mat4&& w)
+{
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            m[i][j] = m[i][j] - w[i][j];
+    return std::move(m);
+}
+
+Mat4 operator*(Mat4& m, Mat4& w)
+{    
+    return Mat4(
+                 m.data[0][0]*w.data[0][0] + m.data[0][1]*w.data[1][0] + m.data[0][2]*w.data[2][0] + m.data[0][3]*w.data[3][0],
+                 m.data[0][0]*w.data[0][1] + m.data[0][1]*w.data[1][1] + m.data[0][2]*w.data[2][1] + m.data[0][3]*w.data[3][1],
+                 m.data[0][0]*w.data[0][2] + m.data[0][1]*w.data[1][2] + m.data[0][2]*w.data[2][2] + m.data[0][3]*w.data[3][2],
+                 m.data[0][0]*w.data[0][3] + m.data[0][1]*w.data[1][3] + m.data[0][2]*w.data[2][3] + m.data[0][3]*w.data[3][3],
+
+                 m.data[1][0]*w.data[0][0] + m.data[1][1]*w.data[1][0] + m.data[1][2]*w.data[2][0] + m.data[1][3]*w.data[3][0],
+                 m.data[1][0]*w.data[0][1] + m.data[1][1]*w.data[1][1] + m.data[1][2]*w.data[2][1] + m.data[1][3]*w.data[3][1],
+                 m.data[1][0]*w.data[0][2] + m.data[1][1]*w.data[1][2] + m.data[1][2]*w.data[2][2] + m.data[1][3]*w.data[3][2],
+                 m.data[1][0]*w.data[0][3] + m.data[1][1]*w.data[1][3] + m.data[1][2]*w.data[2][3] + m.data[1][3]*w.data[3][3],
+
+                 m.data[2][0]*w.data[0][0] + m.data[2][1]*w.data[1][0] + m.data[2][2]*w.data[2][0] + m.data[2][3]*w.data[3][0],
+                 m.data[2][0]*w.data[0][1] + m.data[2][1]*w.data[1][1] + m.data[2][2]*w.data[2][1] + m.data[2][3]*w.data[3][1],
+                 m.data[2][0]*w.data[0][2] + m.data[2][1]*w.data[1][2] + m.data[2][2]*w.data[2][2] + m.data[2][3]*w.data[3][2],
+                 m.data[2][0]*w.data[0][3] + m.data[2][1]*w.data[1][3] + m.data[2][2]*w.data[2][3] + m.data[2][3]*w.data[3][3],
+
+                 m.data[3][0]*w.data[0][0] + m.data[3][1]*w.data[1][0] + m.data[3][2]*w.data[2][0] + m.data[3][3]*w.data[3][0],
+                 m.data[3][0]*w.data[0][1] + m.data[3][1]*w.data[1][1] + m.data[3][2]*w.data[2][1] + m.data[3][3]*w.data[3][1],
+                 m.data[3][0]*w.data[0][2] + m.data[3][1]*w.data[1][2] + m.data[3][2]*w.data[2][2] + m.data[3][3]*w.data[3][2],
+                 m.data[3][0]*w.data[0][3] + m.data[3][1]*w.data[1][3] + m.data[3][2]*w.data[2][3] + m.data[3][3]*w.data[3][3]
+
+            );
+}
+
+Mat4 operator*(Mat4&& m, Mat4& w)
+{ 
+    for(int i=0; i<4; i++)
+    {
+       float temp[4] = {0};
+       for(int j=0; j<4; j++)
+        {
+            for(int k=0; k<4; k++)
+                temp[j] += m.data[i][k]*w.data[k][j];
+        }
+        for(int x=0; x<4; x++) 
+            m.data[i][x] = temp[x];    
+    }
+
+    return std::move(m);
+}
+
+Mat4 operator*(Mat4& m, Mat4&& w)
+{
+    for(int i=0; i<4; i++)
+    { 
+        float temp[4] = {0};
+        for(int j=0; j<4; j++)
+        {
+            for(int k=0; k<4; k++)
+                temp[j] += m.data[j][k]*w.data[k][i];
+        }
+        for(int x=0; x<4; x++)
+            w.data[x][i] = temp[x];
+   }
+
+    return std::move(w);
+}
+
+Mat4 operator*(Mat4&& m, Mat4&& w)
+{ 
+    for(int i=0; i<4; i++)
+    {
+       float temp[4] = {0};
+       for(int j=0; j<4; j++)
+        {
+            for(int k=0; k<4; k++)
+            {
+                temp[j] += m.data[i][k]*w.data[k][j];
+            }
+        }
+        for(int x=0; x<4; x++) 
+            m.data[i][x] = temp[x];    
+    }
+
+    return std::move(m);
+}
+
+Vec4 operator*(Mat4& m, Vec4& v)
+{
+    return Vec4(
+                v[0]*m.data[0][0] + v[1]*m.data[0][1] + v[2]*m.data[0][2] + v[3]*m.data[0][3],
+                v[0]*m.data[1][0] + v[1]*m.data[1][1] + v[2]*m.data[1][2] + v[3]*m.data[1][3],
+                v[0]*m.data[2][0] + v[1]*m.data[2][1] + v[2]*m.data[2][2] + v[3]*m.data[2][3],
+                v[0]*m.data[3][0] + v[1]*m.data[3][1] + v[2]*m.data[3][2] + v[3]*m.data[3][3]
+            );
+}
+
+Vec4 operator*(Mat4& m, Vec4&& v)
+{
+    float temp[4] = {0};
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            temp[i] += v[j]*m.data[i][j];
+        }
+    }
+    for(int i=0; i<4; i++)
+        v[i] = temp[i];
+
+    return std::move(v);
+}
+
+Vec4 operator*(Mat4&& m, Vec4& v)
+{
+  
+    return Vec4(
+                v[0]*m.data[0][0] + v[1]*m.data[0][1] + v[2]*m.data[0][2] + v[3]*m.data[0][3],
+                v[0]*m.data[1][0] + v[1]*m.data[1][1] + v[2]*m.data[1][2] + v[3]*m.data[1][3],
+                v[0]*m.data[2][0] + v[1]*m.data[2][1] + v[2]*m.data[2][2] + v[3]*m.data[2][3],
+                v[0]*m.data[3][0] + v[1]*m.data[3][1] + v[2]*m.data[3][2] + v[3]*m.data[3][3]
+            );
+}
+
+Vec4 operator*(Mat4&& m, Vec4&& v)
+{
+    float temp[4] = {0};
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            temp[i] += v[j]*m.data[i][j];
+        }
+    }
+    for(int i=0; i<4; i++)
+        v[i] = temp[i];
+
+    return std::move(v);
+}
+
+std::ostream& operator<<(std::ostream& os, const Mat4& m)
+{
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
         {
             os << m.data[i][j] <<", ";
         }
--- incsrc/Mathril.h
+++ incsrc/Mathril.h
@@ -104,5 +104,50 @@ class Mat2
         friend Vec2 operator*(Mat2&& m, Vec2& v);
         friend Vec2 operator*(Mat2&& m, Vec2&& v);
 
-        friend std::ostream& operator<<(std::ostream& os, const Mat2& v);
+        friend std::ostream& operator<<(std::ostream& os, const Mat2& m);
+};
+
+class Mat4
+{
+    private:
+         float** data;
+    public:
+         Mat4();
+         Mat4(
+                 float m00, float m01, float m02, float m03,
+                 float m10, float m11, float m12, float m13,
+                 float m20, float m21, float m22, float m23,
+                 float m30, float m31, float m32, float m33
+             );
+
+         Mat4(Mat4&);
+         Mat4(Mat4&&);
+         ~Mat4();
+
+         Mat4& operator=(Mat4&);
+         Mat4& operator=(Mat4&&);
+
+         float* operator[](int);
+
+         friend Mat4 operator+(Mat4& m, Mat4& w);
+         friend Mat4 operator+(Mat4& m, Mat4&& w);
+         friend Mat4 operator+(Mat4&& m, Mat4& w);
+         friend Mat4 operator+(Mat4&& m, Mat4&& w);
+
+         friend Mat4 operator-(Mat4& m, Mat4& w);
+         friend Mat4 operator-(Mat4& m, Mat4&& w);
+         friend Mat4 operator-(Mat4&& m, Mat4& w);
+         friend Mat4 operator-(Mat4&& m, Mat4&& w);
+
+         friend Mat4 operator*(Mat4& m, Mat4& w);
+         friend Mat4 operator*(Mat4& m, Mat4&& w);
+         friend Mat4 operator*(Mat4&& m, Mat4& w);
+         friend Mat4 operator*(Mat4&& m, Mat4&& w);
+
+         friend Vec4 operator*(Mat4& m, Vec4& w);
+         friend Vec4 operator*(Mat4& m, Vec4&& w);
+         friend Vec4 operator*(Mat4&& m, Vec4& w);
+         friend Vec4 operator*(Mat4&& m, Vec4&& w);
+
+         friend std::ostream& operator<<(std::ostream& os, const Mat4& m);
 };
